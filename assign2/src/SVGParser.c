0b6e05d6a12ea5560037fe902e09fdaa60d79f2c
will <wpringle@uoguelph.ca>
2020-02-22 18:58:36 -0500

    reverted to an older version

--- assign2/src/SVGParser.c
+++ assign2/src/SVGParser.c
@@ -1772,14 +1772,12 @@ Circle* parseCircle(xmlNode* cur_node) {
 			
 		} else if(!strcmp(attrName, "r")) {
 			r = strtof(cont, &end);
-			
+			isR = true;
 			
 			// check if r < 0
 			if(r < 0) {
 //				puts("BAD2 - invalid r < 0");
 				
-				
-				freeList(circle->otherAttributes);
 				free(circle);
 				return NULL;
 			}
